8227ee03dc90733f6eb35ab96cd705aac5edf9d0
Tristan Chambers <Tristan.Chambers@hotmail.com>
2014-04-30 15:52:50 -0600

    Created the quest log functions.

--- WvsMapleNpcs/QuestLog.cs
+++ WvsMapleNpcs/QuestLog.cs
@@ -1,6 +1,70 @@
-﻿namespace WvsGame.Maple.Scripting
+﻿using System.Collections.Generic;
+namespace WvsGame.Maple.Scripting
 {
     public class QuestLog
     {
+
+        private List<int> _startedQuests;
+        private List<int> _completedQuests;
+
+        public QuestLog()
+        {
+            _startedQuests = new List<int>();
+            _completedQuests = new List<int>();
+        }
+
+        public int[] StartedQuests
+        {
+            get { return _startedQuests.ToArray(); }
+        }
+
+        public int[] CompletedQuests
+        {
+            get { return _completedQuests.ToArray(); }
+        }
+
+        public bool HasStarted(int ID)
+        {
+            return _startedQuests.Contains(ID);
+        }
+
+        public bool HasCompleted(int ID)
+        {
+            return _completedQuests.Contains(ID);
+        }
+
+        public bool StartQuest(int ID)
+        {
+            bool started = false;
+            if (!_startedQuests.Contains(ID))
+            {
+                _startedQuests.Add(ID);
+                started = true;
+            }
+            return started;
+        }
+
+        public bool AbandonQuest(int ID)
+        {
+            bool existed = false;
+            if (HasStarted(ID) && !HasCompleted(ID))
+            {
+                _startedQuests.Remove(ID);
+            }
+            return existed;
+        }
+
+        public bool CompleteQuest(int ID)
+        {
+            bool completed = false;
+            if (HasStarted(ID) && !HasCompleted(ID))
+            {
+                _startedQuests.Remove(ID);
+                _completedQuests.Add(ID);
+                completed = true;
+            }
+            return completed;
+        }
+
     }
 }
